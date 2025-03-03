bd501ac8dbde506abb82a0d6445ccc956bf35cce
Mukesh <hey.mukesh272921@gmail.com>
2025-03-03 16:14:58 +0530

    feat: bigram model using nn

--- main.py
+++ main.py
@@ -2,43 +2,44 @@ import torch
 
 words = open("names.txt", "r").read().splitlines()
 
-# list of all the chars in the dataset
-chars = sorted(list(set(''.join(words))))
-# char to int mapping
+chars = sorted(set("".join(words)))
+
 stoi = {s: i+1 for i, s in enumerate(chars)}
-stoi["."] = 0
+stoi['.'] = 0
 
 itos = {i: s for s, i in stoi.items()}
 
-# 2d model representing the entire dataset
-# dataset[row, col] represent the frequency of that pair
-# `row` and `col` are the integer mappings of the chars
-dataset = torch.zeros(27, 27, dtype=torch.float32)
+xs, ys = [], []
 
 for word in words:
-    bigram = ["."] + list(word) + ["."]
+    for ch1, ch2 in zip(word[0], word[1:]):
+        idx1 = stoi[ch1]
+        idx2 = stoi[ch2]
 
-    # converts the word into pairs and increments the freq in the dataset tensor
-    for ch1, ch2 in zip(bigram, bigram[1:]):
-        dataset[stoi[ch1], stoi[ch2]] += 1
+        xs.append(idx1)
+        ys.append(idx2)
 
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
 
-for i in range(30):
-    row = 0
-    out = []
+xenc = torch.nn.functional.one_hot(xs, 27).float()
+W = torch.randn((27, 27), requires_grad=True)
 
-    while (True):
-        # convert the freqs into probabilties
-        p = dataset[row]/dataset[row].sum()
-        # get predications based on the above probabilities
-        idx = torch.multinomial(p, 1, True)
-        chstr = itos[int(idx.item())]
+for i in range(100):
+    # forward pass
+    logits = (xenc @ W)
+    counts = logits.exp()
+    prob = counts/counts.sum(dim=1, keepdim=True)
 
-        # loop until special character is reached i.e. end of the string is reached
-        if (chstr == "."):
-            break
+    # calculating loss for the entire dataset
+    loss = -prob[torch.arange(ys.nelement()),
+                 ys].log().mean() + (0.01) * (W**2).mean()
 
-        out.append(chstr)
-        row = int(idx)
+    print(f"iter {i+1}: loss = {loss}")
 
-    print("".join(out))
+    # setting the gradient to be equal to 0
+    W.grad = torch.zeros((27, 27))
+    # backward pass
+    loss.backward()
+    # updating the weights
+    W.data += -30 * W.grad
---
+++ notes.md
@@ -0,0 +1,26 @@
+# notes
+
+## bigram model
+
+1. in bigram model, all the words are split up into pairs of two letters (bigrams) (emma -> (., e), (e, m), (m, m), (m, a), (a, .))
+2. a matrix is formed which contains the frequency of each pair
+3. the matrix is then normalized to get the probability distributions
+4. to predict a word, a loop is ran which starts with `.` character and then samples the next character using `torch.multinomial` function which just returns the next character according to the probability distributions
+5. after getting the next character, same thing is done again but with the next character's row and the loop runs until `.` character is hit again.
+6. negative log likelihood is used as the loss function
+7. a random constant is added to all the counts in the matrix which acts like model smoothing/regularization
+
+## neural network
+
+1. all the words are split up into pairs of two letters and the first element of that pair is appended to `xs` and the second element is appended to the `ys` array. `xs` array is passed to the network and `ys` are the targets
+2. `xs` array is transformed by applying one-hot encoding
+3. a random weight matrix is initialized using normal distribution and forward pass is performed
+4. negative-log likelihood is again used as the loss function and along with it L2 loss used for regularization
+
+   ```py
+   loss = -prob[torch.arange(ys.nelement()), ys].log().mean() + (0.01) * (W**2).mean()
+   ```
+
+5. backward pass is performed and the weights are updated using basic SGD
+
+1.933
